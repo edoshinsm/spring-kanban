93d5b8a0f522ab495fb5fbb8d42643d3b2fdcb15
edoshinsm <sergey.edoshin@gmail.com>
2026-07-09 14:04:04 +0300

    build(data): data.sql файл инициализации

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,31 @@
-insert into projects (name) values ('имя_проекта1'), ('имя_проекта2'), ('имя_проекта3'), ('имя_проекта4');
+insert into users (id, username, name, surname, password_hash, email) values
+(next value for user_id_seq, 'user1', 'сергей', 'едошин', 'hash1', 'user1@mail.com'),
+(next value for user_id_seq, 'user2', 'иван', 'иванов', 'hash2', 'user2@mail.com');
 
-insert into columns (name, project_id) values
-    ('имя_доски1', 1),
-    ('имя_доски2', 1),
-    ('имя_доски_очень_длинное_и_точно_не_вместиться3', 1),
-    ('имя_доски4', 1),
-    ('имя_доски5', 1),
-    ('имя_доски6', 1),
-    ('имя_доски7', 1),
-    ('имя_доски1', 2);
+insert into projects (id, name, created_at) values
+(next value for project_id_seq, 'имя_проекта1', now()),
+(next value for project_id_seq, 'имя_проекта2', now()),
+(next value for project_id_seq, 'имя_проекта3', now()),
+(next value for project_id_seq, 'имя_проекта4', now());
 
-insert into issues(name, description, column_id, created_at) values
-    ('имя_задания', 'некоторое описание задачи представлено тут, некоторое описание задачи представлено тут, некоторое описание задачи представлено тут, некоторое описание задачи представлено тут', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', 'описание такое вот', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', '', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', 'описание\n\n\n\n описание', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', '', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', '', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', '', 1, '2026-01-01 12:00:00'),
-    ('имя_задания', '', 1, '2026-01-01 12:00:00'),
-    ('имя_задания очень длинное и точно не вместиться', '', 2, '2026-01-01 12:00:00'),
-    ('имя_задания', 'описание такое вот', 2, '2026-01-01 12:00:00');
+insert into columns (id, name, project_id, position) values
+(next value for column_id_seq, 'колонка1', 1, 1),
+(next value for column_id_seq, 'колонка2', 1, 2),
+(next value for column_id_seq, 'колонка3', 1, 3),
+(next value for column_id_seq, 'колонка4', 1, 4),
+(next value for column_id_seq, 'колонка5', 1, 5),
+(next value for column_id_seq, 'колонка6', 1, 6),
+(next value for column_id_seq, 'колонка1', 2, 1);
+
+insert into issues (id, name, description, column_id, created_at, status, due_date, creator_id) values
+(next value for issue_id_seq, 'задание1', 'описание', 1, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание2', 'описание', 1, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание3', 'описание', 1, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание4', 'описание', 1, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание1', 'описание', 2, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание2', 'описание', 2, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1),
+(next value for issue_id_seq, 'задание3', 'описание', 2, '2026-01-01 10:00:00', 'OPEN', '2026-02-01 10:00:00', 1);
+
+insert into project_members (id, role, user_id, project_id) values
+(next value for project_members_id_seq, 'ADMIN', 1, 1),
+(next value for project_members_id_seq, 'VIEWER', 2, 1);
