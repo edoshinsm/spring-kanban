ccc31e4a311cddfb9d0893ead5dfea8fd6d05869
edoshinsm <sergey.edoshin@gmail.com>
2026-07-09 14:05:45 +0300

    fix(model): исправил тип Generic'а

--- src/main/java/myApp/repository/ProjectMembersRepository.java
+++ src/main/java/myApp/repository/ProjectMembersRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectMembersRepository extends JpaRepository<Long, EntityProjectMembers> {
+public interface ProjectMembersRepository extends JpaRepository<EntityProjectMembers, Long> {
 }
